496f720760e64d1c1f9b67aecc55fe1aed104efb
Chadi Abi Fadel <chadi@abifadel.net>
2025-05-23 13:34:21 +0300

    yet another app

---
+++ sub_apps/semantic_search.py
@@ -0,0 +1,11 @@
+def semantic_search_redirect():
+    st.title("Semantic Search")
+    st.write("This is the semantic search app")
+
+    st.write("For technical reasons that have to do with libraries, We need to ask you to go to this link:")
+    st.write("[Semantic Search App](https://semanticsearchapp.streamlit.app/)")
+
+    st.write("Once you have completed the demonstration, please return to this app To continue with the exploration")
+
+if __name__ == "__main__":
+    semantic_search_redirect()
