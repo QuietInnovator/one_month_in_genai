c1d74a94a5613b178a8de6ccb85fc702aa258d59
Chadi Abi Fadel <chadi@abifadel.net>
2025-05-26 14:57:59 +0300

    imports fix

--- sub_apps/entity_extraction.py
+++ sub_apps/entity_extraction.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from google import genai
-from google.genai import types
+# from google import genai
+# from google.genai import types
 
 # ------------------ CONFIG ------------------
 def get_gemini_config():
